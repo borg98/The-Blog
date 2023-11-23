791c908d8e3309add41d9e6938b801f1bc9d4987
Martin Borg <borg.martin@outlook.com>
2023-11-23 10:53:19 +0100

    123

---
+++ src/ts/module/blog.ts
@@ -0,0 +1,9 @@
+export class Blog {
+  constructor(
+    public title: string,
+    public timestamp: string,
+    public category: string,
+    public content: string,
+    public author: string
+  ) {}
+}
