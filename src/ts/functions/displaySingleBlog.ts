7eeaf7ad680e6b2fd99acddcd6a1a6962c500d86
Martin Borg <borg.martin@outlook.com>
2023-11-23 13:38:55 +0100

    tillbakaknapp

--- src/ts/functions/displaySingleBlog.ts
+++ src/ts/functions/displaySingleBlog.ts
@@ -14,34 +14,51 @@ export function displaySingleBlog(aBlog: Blog) {
   ) as HTMLDivElement;
   mainContainer.innerHTML = "";
 
+  const blogContainer = document.createElement("div");
+  blogContainer.className = "main-container__blog-container";
+
   const titleTag = document.createElement("h3");
-  titleTag.className = "main-container__heading-container__title";
+  titleTag.className =
+    "main-container__blog-container__heading-container__title";
   titleTag.innerHTML = aBlog.title;
 
   const dateTag = document.createElement("p");
-  dateTag.className = "main-container__heading-container__date";
+  dateTag.className = "main-container__blog-container__heading-container__date";
   dateTag.innerHTML = aBlog.timestamp;
 
   const categoryTag = document.createElement("p");
-  categoryTag.className = "main-container__heading-container__category";
+  categoryTag.className =
+    "main-container__blog-container__heading-container__category";
   categoryTag.innerHTML = aBlog.category;
 
   const contentTag = document.createElement("p");
-  contentTag.className = "main-container__content";
+  contentTag.className = "main-container__blog-container__content";
   contentTag.innerHTML = aBlog.content;
 
   const authorTag = document.createElement("p");
-  authorTag.className = "main-container__author";
+  authorTag.className = "main-container__blog-container__author";
   authorTag.innerHTML = aBlog.author;
 
   const headingContainer = document.createElement("div");
-  headingContainer.className = "main-container__heading-container";
+  headingContainer.className =
+    "main-container__blog-container__heading-container";
+
+  const backButton = document.createElement("buton");
+  backButton.className = "main-container__button";
+
+  const backButtonITag = document.createElement("i");
+  backButtonITag.classList.add("fa-solid", "fa-arrow-left");
+
+  backButton.appendChild(backButtonITag);
 
   headingContainer.appendChild(titleTag);
   headingContainer.appendChild(dateTag);
   headingContainer.appendChild(categoryTag);
 
-  mainContainer?.appendChild(headingContainer);
-  mainContainer?.appendChild(contentTag);
-  mainContainer?.appendChild(authorTag);
+  blogContainer.appendChild(headingContainer);
+  blogContainer.appendChild(contentTag);
+  blogContainer.appendChild(authorTag);
+
+  mainContainer?.appendChild(backButton);
+  mainContainer?.appendChild(blogContainer);
 }
