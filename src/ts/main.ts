97f646bbdac35f90052548f5d9b643693ae10843
Martin Borg <borg.martin@outlook.com>
2023-11-24 09:52:29 +0100

    123

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,7 +1,5 @@
 import "./../scss/style.scss";
-import { dateSort } from "./functions/dateSort";
 import { selectCategories } from "./functions/selectCategories";
-import { blog, displaySingleBlog } from "./functions/displaySingleBlog";
 import { Blog } from "./module/blog";
 import { createBlogpost } from "./functions/createBlogpost";
 import { creatHtml } from "./functions/creatHtml";
@@ -12,25 +10,23 @@ let page = document.body.id;
 switch (page) {
   case "1":
     creatHtml(LoadFromLS(blogPosts));
-    // displaySingleBlog(blog);
     break;
   case "2":
     const submitBlogpost = document.getElementById(
       "formContainer__button__submit"
     ) as HTMLButtonElement;
     submitBlogpost.addEventListener("click", (e) => {
+      const newLoadedList = LoadFromLS(blogPosts);
       e.preventDefault();
       const newBlog = createBlogpost();
-      blogPosts.push(newBlog);
-      console.log(blogPosts);
-      SaveToLS(blogPosts);
+      newLoadedList.push(newBlog);
+      blogPosts = newLoadedList;
+
+      SaveToLS([...blogPosts]);
     });
     break;
   case "3":
     let loadedList: Blog[] = LoadFromLS(blogPosts);
-    /* Call the create html-function and iterate through the new sorted list, dateSortblogPosts,
-    to display the blog posts in descending order, with the newest blog post appearing first. */
-
     selectCategories(loadedList);
     creatHtml(blogPosts);
     break;
